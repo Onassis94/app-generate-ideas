8b7e09d744c716e2dd19a11ec90c7ee487587ab9
onassis28 <onassis.sowah@gmail.com>
2023-02-09 16:31:57 +0100

    working state

--- src/app.jsx
+++ src/app.jsx
@@ -1,43 +1,122 @@
 import * as React from 'react';
-import {createRoot} from 'react-dom/client';
+import axios from 'axios';
+import { createRoot } from 'react-dom/client';
+import Button from '@mui/material/Button';
+import { LinearProgress, TextField } from '@mui/material';
+import { Container, Box } from '@mui/system';
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
+// use user input and concantenate user input
+const prompt = 'generate 3 ideas for a YouTube video';
 
-  await miro.board.viewport.zoomTo(stickyNote);
-}
+// Make the API call
 
 const App = () => {
-  React.useEffect(() => {
-    addSticky();
-  }, []);
+	// React.useEffect(() => {
+	// 	generateIdeas();
+	// }, []);
+	const [ideas, setIdeas] = React.useState([]);
+	const [isActivated, setIsActivated] = React.useState(false);
+	const [promptData, setPromptData] = React.useState('');
+	const [linear, setLinear] = React.useState(false);
+	const options = {
+		method: 'POST',
+		headers: {
+			accept: 'application/json',
+			'content-type': 'application/json',
+			authorization:
+				'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_P49rjoPPIDRzD4edjuDzrDTaGOs',
+		},
+		body: JSON.stringify({
+			data: { content: ideas, shape: 'square' },
+			position: { x: 100, y: 100 },
+		}),
+	};
+	function addStickyNote() {
+		ideas.forEach((idea) => {
+			fetch('https://api.miro.com/v2/boards/uXjVPv6nqgY%3D/sticky_notes', {
+				method: 'POST',
+				headers: {
+					accept: 'application/json',
+					'content-type': 'application/json',
+					authorization:
+						'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_P49rjoPPIDRzD4edjuDzrDTaGOs',
+				},
+				body: JSON.stringify({
+					data: { content: idea, shape: 'square' },
+					position: { x: 100, y: 100 },
+				}),
+			})
+				.then((response) => response.json())
+				.then((data) => console.log(data));
+		});
+	}
+	function generateIdeas() {
+		setLinear(true);
+		axios
+			.post(
+				'https://api.openai.com/v1/engines/text-davinci-002/completions',
+				{
+					prompt: promptData,
+					max_tokens: 1024,
+					temperature: 0.7,
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer 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`,
+					},
+				}
+			)
+			.then((response) => {
+				const data = checkNewLine(response.data.choices[0].text);
+				console.log(data);
+				// Print the generated ideas
+				setIdeas(data);
+				setIsActivated(true);
+				setLinear(false);
+			})
+			.catch((error) => {
+				setIdeas(error);
+			});
+	}
 
-  return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
-      </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
-      </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
-        >
-          Read the documentation
-        </a>
-      </div>
-    </div>
-  );
+	function checkNewLine(ideas) {
+		let newLine = ideas.split('\n');
+		newLine = newLine.filter((idea) => idea !== '');
+		console.log(newLine);
+		return newLine;
+	}
+
+	return (
+		<Box>
+			<Container maxWidth='md'>
+				<Box mt={20}>
+					<Box mb={5} textAlign='center' fontSize={30}>
+						Web Tool to add Computer-generated ideas to Miro Boards{' '}
+					</Box>
+					<TextField
+						onChange={(e) => {
+							setPromptData(e.target.value);
+						}}
+						multiline
+						minRows={4}
+						fullWidth
+					/>
+				</Box>
+				<Box mt={10} display='flex' gap={6} justifyContent='center'>
+					<Button variant='contained' onClick={addStickyNote}>
+						click me to add sticky notes
+					</Button>
+					<Button variant='contained' onClick={generateIdeas}>
+						click me to generate ideas
+					</Button>
+				</Box>
+				<Box mt={5}>
+					{isActivated ? 'your ideas have been generated' : <LinearProgress />}
+				</Box>
+			</Container>
+		</Box>
+	);
 };
 
 const container = document.getElementById('root');
