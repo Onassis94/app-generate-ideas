f8d29b1f61f2c882664759fcd9836cc4dc287e01
onassis28 <onassis.sowah@gmail.com>
2023-04-02 21:08:59 +0200

    code refactor

--- src/components/how-to-use.jsx
+++ src/components/how-to-use.jsx
@@ -14,6 +14,10 @@ const HowToUse = () => {
 				<h2>Instructions on how to use this tool</h2>
 				<ol>
 					<li>
+						Get GPT-3 API access token from openAI, navigate to /gpt auth or
+						click the navigation link AI API and put in the token
+					</li>
+					<li>
 						<p>Get access token from Miro</p>
 						<ul>
 							<li>Create a Miro account if you don't have one already</li>
